2b62d3a85ad4831ce0734838d2b3c0e2f5fd2b8b
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2022-04-15 15:40:26 -0400

    copy showPrayers.sh into showPrayersMacOS.sh and change date to GNU date

---
+++ showPrayersMacOS.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+#you should keep it
+today=$(date +%s)
+
+# change this - use URL for help
+# https://www.latlong.net/
+lat='28.596769'
+long='-81.203430'
+
+# change this - use URL for help
+# https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+country="US"
+
+# method" (number) -
+# A prayer times calculation method. Methods identify various schools of thought about how to compute the timings. If not specified, it defaults to the closest authority based on the location or co-ordinates specified in the API call. This parameter accepts values from 0-12 and 99, as specified below:
+# * 0 - Shia Ithna-Ansari
+# 1 - University of Islamic Sciences, Karachi
+# 2 - Islamic Society of North America
+# 3 - Muslim World League
+# 4 - Umm Al-Qura University, Makkah
+# 5 - Egyptian General Authority of Survey
+# 7 - Institute of Geophysics, University of Tehran
+# 8 - Gulf Region
+# 9 - Kuwait
+# 10 - Qatar
+# 11 - Majlis Ugama Islam Singapura, Singapore
+# 12 - Union Organization islamic de France
+# 13 - Diyanet İşleri Başkanlığı, Turkey
+# 14 - Spiritual Administration of Muslims of Russia
+# 15 - Moonsighting Committee Worldwide (also requires shafaq paramteer)
+# 99 - Custom. See https://aladhan.com/calculation-methods
+
+# change this - use information abbove
+method="2" #method 2 for Americas for example
+
+
+# adjust the Hijri date- change it if you need
+adjustment="1"
+
+output="$HOME/showPrayers/prayers.json"
+
+# The api can be found for free
+curl "http://api.aladhan.com/v1/timings/$today?latitude=$lat&longitude=$long&method=$method&adjustment=$adjustment" -s -o $output
+
+
+
+# Parsing the data for the five salawat
+prayers="$HOME/showPrayers/prayers.json"
+fajr=$(jq ".data.timings.Fajr" $prayers | bc | awk '{$1=$1};1')
+sunrise=$(jq ".data.timings.Sunrise" $prayers | bc | awk '{$1=$1};1')
+dhuhr=$(jq ".data.timings.Dhuhr" $prayers | bc | awk '{$1=$1};1')
+asr=$(jq ".data.timings.Asr" $prayers | bc | awk '{$1=$1};1')
+maghrib=$(jq ".data.timings.Maghrib" $prayers | bc | awk '{$1=$1};1')
+isha=$(jq ".data.timings.Isha" $prayers | bc | awk '{$1=$1};1')
+day=$(jq ".data.date.hijri.weekday.en" $prayers | bc | awk '{$1=$1};1')
+daynumber=$(jq ".data.date.hijri.day" $prayers | bc | awk '{$1=$1};1')
+month=$(jq ".data.date.hijri.month.en" $prayers | bc | awk '{$1=$1};1')
+year=$(jq ".data.date.hijri.year" $prayers | bc | awk '{$1=$1};1')
+
+nextprayer=""
+# Get the current time
+currenttime=$(date +%H:%M)
+
+# For each prayer, two variables are used, one to be printed as the name of the prayer (nextprayer), 
+# and the other variable (time) to be used in the calculation of the remaining time (nextTime)
+if [[ $currenttime > $fajr && $currenttime < $dhuhr ]]; then
+    nextprayer=$(echo "Dhuhr")
+    nextTime=$dhuhr
+
+elif [[ $currenttime > $dhuhr && $currenttime < $asr ]]; then
+    nextprayer=$(echo "Asr")
+    nextTime=$asr
+
+elif [[ $currenttime > $asr && $currenttime < $maghrib ]]; then
+    nextprayer=$(echo "Maghrib")
+    nextTime=$maghrib
+
+elif [[ $currenttime > $maghrib && $currenttime < $isha ]]; then
+    nextprayer=$(echo "Isha")
+    nextTime=$isha
+
+elif [[ $currenttime > $isha || $currenttime < $fajr ]]; then
+    nextprayer=$(echo "Fajr")
+    nextTime=$fajr
+fi
+
+# Calculate the remaining time to the next prayer (or iftar in ramadan and the fast duration is ramadan)
+remain=$(gdate -u -d @$(($(gdate -d "$nextTime" "+%s") - $(gdate -d "$currenttime" "+%s"))) "+%H:%M")
+fast=$(gdate -u -d @$(($(gdate -d "$maghrib" '+%s') - $(gdate -d "$fajr" '+%s'))) '+%H:%M')
+Tofast=$(gdate -u -d @$(($(gdate -d "$maghrib" '+%s') - $(gdate -d "$currenttime" '+%s'))) '+%H:%M')
+
+# Sending the salawat to the stdout
+printf "\nTime: $currenttime\n"
+printf "\n🕌 Prayer times for the day: $day \n$daynumber-$month-$year\n"
+printf "\n۞ Fajr\t\t$fajr\n"
+printf "۞ Sunrise\t$sunrise\n"
+printf "۞ Dhuhr\t\t$dhuhr\n"
+printf "۞ Asr\t\t$asr\n"
+printf "۞ Maghrib\t$maghrib\n"
+printf "۞ Isha\t\t$isha\n\n"
+
+printf "\n🕌 Next prayer ۩ $nextprayer ۩ $nextTime\n"
+printf "Remaining time $remain\n\n"
+printf "Duration of fasting $fast\n"
+printf "Time left until breakfast $Tofast\n\n"
